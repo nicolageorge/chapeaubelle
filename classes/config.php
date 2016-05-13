0ecb3a4d77264137fb11356cf7ad4a21e35b7383
George Nicola <nicola.george2040@gmail.com>
2016-05-13 18:11:15 +0300

    last commit of evening

--- classes/config.php
+++ classes/config.php
@@ -1,4 +1,9 @@
 <?php
+define( 'DB_NAME', 'chapeaubelle' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASS', 'asd123' );
+define( 'DB_HOST', 'localhost' );
+
 $config = array();
 $config['base_url'] = "http://chapeaubelle.dev";
 
