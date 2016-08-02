d4770628438ae1900afd06b0c16b515402ed1708
George Nicola <nicola.george2040@gmail.com>
2016-08-02 18:23:20 +0300

    refactor

--- controllers/home_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once( __ROOT__ . "/classes/controller.php" );
-
-class HomeController extends TreeController{
-
-	public function indexAction(){
-		$this->view->render( "/views/pages/home.php", [ "var1" => "var1 val" ] );
-	}
-}
